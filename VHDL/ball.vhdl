95c5dc825fe37846e27d47c7a54e2e5655267fc3
Gabriel Tomaz de Lemos Garcia <100324929+El-Tomaz@users.noreply.github.com>
2025-06-26 00:32:54 +0000

    vhdl:Adicionado arquivos VHDL!

---
+++ VHDL/ball.vhdl
@@ -0,0 +1,10 @@
+
+-- how colision works:
+-- on vertical colision (colision with paddles) X velocity of the ball is inverted, Y velocity isnt changed
+-- on horizontal colision (colision with top and botton of frame) X veloociity is constant, Y velocity is inverted
+
+-- how are the clocks used:
+-- pixel clock is used to output pixels of the ball
+-- vsync is used to 
+
+entity ball is  
